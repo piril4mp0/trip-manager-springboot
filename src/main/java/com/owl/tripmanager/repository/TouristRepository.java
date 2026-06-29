76020acd0e396c8e3d63e000b43fb58911f11510
piril4mp0 <muriloxd23@gmail.com>
2026-06-28 21:00:57 -0300

    chore(config): disable sql query logs in dev

--- src/main/java/com/owl/tripmanager/repository/TouristRepository.java
+++ src/main/java/com/owl/tripmanager/repository/TouristRepository.java
@@ -1,12 +1,11 @@
 package com.owl.tripmanager.repository;
 
-import com.owl.tripmanager.model.Tourist;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import java.util.Optional;
 
-@Repository
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.owl.tripmanager.model.Tourist;
+
 public interface TouristRepository extends JpaRepository<Tourist, Long> {
     Optional<Tourist> findByDocument(String document);
     boolean existsByDocument(String document);
--- src/main/java/com/owl/tripmanager/repository/TravelExpenseRepository.java
+++ src/main/java/com/owl/tripmanager/repository/TravelExpenseRepository.java
@@ -1,11 +1,11 @@
 package com.owl.tripmanager.repository;
 
-import com.owl.tripmanager.model.TravelExpense;
 import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.owl.tripmanager.model.TravelExpense;
+
 public interface TravelExpenseRepository extends JpaRepository<TravelExpense, Long> {
     List<TravelExpense> findByTripId(Long tripId);
 }
